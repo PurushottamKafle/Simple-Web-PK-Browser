447900f6b0cfecec9758fa9c05c9ca77a6c57fc8
Purushottam Kafle <32263675+PurushottamKafle@users.noreply.github.com>
2018-04-30 18:59:02 +0545

    Create second.java

---
+++ second.java
@@ -0,0 +1,17 @@
+package pksoft;
+
+import java.awt.*;
+
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public  class second {
+	public static void main(String[] args) {
+		first win = new first();
+		win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				
+	}
+
+}
